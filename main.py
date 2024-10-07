fe4dd46bd4d2e5884627b2d4bf382aff14a87cc7
Sandip Khaire <sandipkhaire007@gmail.com>
2024-10-07 20:09:57 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    df={"station":station,
+        "date":date,
+        "temperature":24}
+    return df
+
+
+if __name__=="__main__":
+    app.run(debug=True)
